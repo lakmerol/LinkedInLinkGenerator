5595c6aa9d1f65366201f55d5a0e2b159da46b73
Erol <lakmerol@gmail.com>
2023-01-08 18:33:04 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedInLinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Titles = { "new graduate", "new grad", "yeni mezun","mezun" , "intern", "junior","jr" };
+            string[] JobTitles = { "Software Developer", "Web Developer", "Application Developer", ".NET Developer", "C# Developer", "ASP.NET Developer", ".NET Software Engineer", "Full Stack%20Developer" ,".NET CORE" };
+            string[] Cities = { "Izmır", "Istanbul", "Ankara", "Antalya" };
+            string ChangedLink = $"https://www.linkedin.com/jobs/search/?&keywords=JobTitles%20Titles&location=Cities%2C%20Turkey&refresh=true";
+            StringBuilder LinkSb = new StringBuilder();
+       
+            for (int i = 0; i < Titles.Count(); i++)
+            {
+             Titles[i] = Replacer(Titles[i]);
+                
+            }
+            for ( int i = 0; i < JobTitles.Count(); i++)
+            {
+               JobTitles[i] = Replacer(JobTitles[i]);
+               JobTitles[i] = Replacer(JobTitles[i]);
+            }
+
+
+            for (int i = 0; i < JobTitles.Count(); i++)
+            {
+                string link = ChangedLink;
+              
+                link = link.Replace("JobTitles", JobTitles[i]);
+                for (int x = 0; x < Titles.Count(); x++)
+                {
+                    string JChangeLink = link;
+                    JChangeLink = link.Replace("Titles", Titles[x]);
+                   
+                    for (int y = 0; y < Cities.Count(); y++)
+                    {
+                        string CChangeLink = JChangeLink;
+                        CChangeLink = JChangeLink.Replace("Cities", Cities[y]);
+                        LinkSb.AppendLine(CChangeLink);
+                    }
+                }
+            }
+            Console.WriteLine(LinkSb);
+            Console.ReadLine();
+        }
+        public static string Replacer(string word)
+        {
+            if (word.Contains(" ")) word = word.Replace(" ", "%20");
+
+           return word;
+        }
+    }
+}
